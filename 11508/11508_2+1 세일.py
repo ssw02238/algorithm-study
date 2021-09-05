34ecb7b3be251c4ed028a11c0a54b7fc01ef9074
ssw02238 <ssw0223820@gmail.com>
2021-09-05 10:13:19 +0900

    11508 answer

--- 11508/11508_2+1 세일.py
+++ 11508/11508_2+1 세일.py
@@ -6,4 +6,18 @@ N = int(input())
 prices = [int(input()) for _ in range(N)]
 
 answer = 0
+prices.sort()
+free = N // 3
+answer = sum(prices)
+for i in range(free):
+    answer -= prices[(i+1)*-3]
+print(answer)
 
+
+
+
+
+# answer = sum(prices)
+# for i in range(free):
+#     answer -= prices[i]
+# print(answer)
