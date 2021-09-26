18e0d470659bf8747d351055c097165ca8274595
ssw02238 <ssw0223820@gmail.com>
2021-09-26 20:44:32 +0900

    13549 숨바꼭질 answer

---
+++ 13549_숨바꼭질3/answer.py
@@ -0,0 +1,21 @@
+N = 4
+K = 6
+
+def solution(start):
+    if N < K and N == 0:
+        return K
+    elif N > K and K == 0:
+        return N
+    global queue
+    while queue:
+        start = queue.pop(0)
+        if start[0] == K:
+            return start[1]
+        queue.append([2 * start[0], start[1]])
+        queue.append([start[0]-1, start[1]+1])
+        queue.append([start[0]+1, start[1]+1])
+
+
+
+queue = [[N, 0]]
+print(solution([N, 0]))
