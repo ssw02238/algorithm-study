07e62b3d06a0582d2f3d6a8bef3b15a7dd203335
ssw02238 <ssw0223820@gmail.com>
2021-10-08 22:58:18 +0900

    12904 answer

--- 12904_A와 B/12904.py
+++ 12904_A와 B/12904.py
@@ -7,24 +7,33 @@ T = input()
 result = 0
 queue = [S]
 tmp = S
-while len(tmp) <= len(T):
+while queue and len(tmp) <= len(T):
     tmp = queue.pop(0)
     # 규칙 1
     a = tmp + 'A'
     # 규칙 2
     b = list(reversed(list(tmp)))
     b.append('B')
-    b1 = ''
-    for i in range(len(b)):
-        b1 += b[i]
+    b1 = ''.join(b)
 
     # 확인
     if a == T or b1 == T:
         result = 1
         break
     else:
-        queue.append(a)
-        queue.append(b1)
+        ar = list(a)
+        ar.reverse()
+        ar = ''.join(ar)
+
+        br = list(b1)
+        br.reverse()
+        br = ''.join(br)
+        # print(ar, br)
+
+        if a in T or ar in T:
+            queue.append(a)
+        if b1 in T or br in T:
+            queue.append(b1)
 print(result)
 
 
