2f6a20b2aff3564e8bd7d35a20637f7c191f0ecd
ssw02238 <ssw0223820@gmail.com>
2021-10-21 20:45:00 +0900

    가장 큰 수

---
+++ programmers_가장 큰 수/answer.py
@@ -0,0 +1,9 @@
+numbers = [3, 30, 34, 5, 9]
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    # numbers.sort(key=lambda x: x * 3, reverse=True)
+    # print(numbers)
+    return str(int(''.join(numbers)))
+
+print(solution(numbers))
